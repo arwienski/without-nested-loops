cd2381b2af40130c72786a6812f9ac9c2cb7df8f
arwienski <95185195+arwienski@users.noreply.github.com>
2022-03-04 15:05:05 +0500

    Update WNL.ts

--- WNL.ts
+++ WNL.ts
@@ -3,27 +3,26 @@ function matrix(n: number) {
     
     const M = new Array<Array<number>>(n);   // создаем пустой массив 
   
-    let startX = 0, endX = 0;       // ограничители координат для оси Х
-    let startY = 0, endY = 0;       // ограничители координат для оси Y
+    let startX = 0, endX = 1;       // ограничители координат для оси Х
+    let startY = 0, endY = 1;       // ограничители координат для оси Y
 
     let y=0, x=0;                       // сами координаты
     
     for (let i = 0; i < n * n; i++) {
         if (i < n)
-        M[i] = new Array<number>(n); // добавил в один цикл без методов fill и т.д., чтобы уложиться в 81 итерацию цикла
+        M[i] = new Array<number>(n);  // добавил в один цикл без методов fill и т.д., чтобы уложиться в 81 итерацию цикла
 
         M[y][x]=i+1;
 
-        if (y === startY &&( x < n - endX - 1))  
+        if (y === startY &&( x < n - endX))  
             x++;
-        else if ((x === n - endX - 1) && (y < n - endY - 1))
+        else if ((x === n - endX) && (y < n - endY))
             y++;
-        else if ((y === n - endY - 1) && (x > startX))
+        else if ((y === n - endY) && (x > startX))
             x--;
         else y--;
-        
-        // когда прошли периметр, заходим внутрь:
-        if ((y === startY + 1) && (x === startX) && (startX != n - endX - 1)) {
+
+        if ((y === startY + 1) && (x === startX) && (startX !== n - endX)) {
             startX++;
             startY++;
             endX++;
